e643c3407b82dd5ba20706f48acdfd0c735f1032
Matthew Briggs <matthew.briggs04@gmail.com>
2026-02-18 21:40:56 -0500

    refactor fn structure

--- lidar.py
+++ lidar.py
@@ -3,8 +3,17 @@ import glob
 
 def load_velodyne_bin(bin_path: str) -> np.ndarray:
     pc = np.fromfile(bin_path, dtype=np.float32)
+    # reshape into (x, y, z, reflectance)
+    # reflectance = intensity = strength of LiDAR returned
     pc = pc.reshape(-1, 4)
     return pc
+
+def log_pc(pc: np.ndarray):
+    print("Pointcloud shape:", pc.shape)
+    print("x/front-back range (meters):", pc[:,0].min(), pc[:,0].max())
+    print("y/left-right range (meters):", pc[:,1].min(), pc[:,1].max())
+    print("z/up-down range (meters):", pc[:,2].min(), pc[:,2].max())
+    print("reflectance range:", pc[:,3].min(), pc[:,3].max())
 
 if __name__ == "__main__":
     train_velo_path = "data/training/velodyne"
@@ -12,9 +21,4 @@ if __name__ == "__main__":
 
     print(f"Found {len(lidar_files)} training LiDAR files")
     pc = load_velodyne_bin(lidar_files[0])
-
-    print("Pointcloud shape:", pc.shape)
-    print("x range:", pc[:,0].min(), pc[:,0].max())
-    print("y range:", pc[:,1].min(), pc[:,1].max())
-    print("z range:", pc[:,2].min(), pc[:,2].max())
-    print("reflectance range:", pc[:,3].min(), pc[:,3].max())
+    log_pc(pc)
